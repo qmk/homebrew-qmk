7ddfb6d1ddada2691a9c7b9ce64996e68369d303
Zach White <skullydazed@gmail.com>
2021-01-11 20:01:18 -0800

    update qmk_cli to 0.0.39 and update deps

--- qmk.rb
+++ qmk.rb
@@ -3,8 +3,8 @@ class Qmk < Formula
 
   desc "Quantum Mechanical Keyboard (QMK) Firmware"
   homepage "https://docs.qmk.fm/"
-  url "https://files.pythonhosted.org/packages/b0/f3/6bb6a5fd5ea95a965ea9dc0a3d36a638c1edaeb8da76bf171acb4129cb2c/qmk-0.0.37.tar.gz"
-  sha256 "c10a1b18a9a9ecfa1a188a4174ed97fcb9b46b5fb706af6acdf879cfdef1af24"
+  url "https://files.pythonhosted.org/packages/18/e0/58ac034a7c4761d2cbc0917f31af1cc02ce0ff388169f2a4aaa341648446/qmk-0.0.39.tar.gz"
+  sha256 "21532ca4daa40c01f0727a162f9b67e5cd070cacfc636739b87460065a5f1863"
 
   depends_on "avrdude"
   depends_on "bootloadhid"
@@ -26,14 +26,24 @@ class Qmk < Formula
     sha256 "de0e1282330940d52ea92a80fea2e4b9e0da1932aaa570f84d268939d1897b04"
   end
 
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/f0/cb/80a4a274df7da7b8baf083249b0890a0579374c3d74b5ac0ee9291f912dc/attrs-20.3.0.tar.gz"
+    sha256 "832aa3cde19744e49938b91fea06d69ecb9e649c93ba974535d08ad92164f700"
+  end
+
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/1f/bb/5d3246097ab77fa083a61bd8d3d527b7ae063c7d8e8671b1cf8c4ec10cbe/colorama-0.4.4.tar.gz"
     sha256 "5941b2b48a20143d2267e95b1c2a7603ce057ee39fd88e7329b0c292aa16869b"
   end
 
   resource "coverage" do
-    url "https://files.pythonhosted.org/packages/da/50/4c0c93ea67c8b42aa700cfbdedd64ea5ac5e7108ba14e3e744f57309304b/coverage-5.3.tar.gz"
-    sha256 "280baa8ec489c4f542f8940f9c4c2181f0306a8ee1a54eceba071a449fb870a0"
+    url "https://files.pythonhosted.org/packages/40/05/2c1d1405edeec38114abcd404f15a35a41029b89d0514aa8ad11ffcbde81/coverage-5.3.1.tar.gz"
+    sha256 "38f16b1317b8dd82df67ed5daa5f5e7c959e46579840d77a67a4ceb9cef0a50b"
+  end
+
+  resource "dotty-dict" do
+    url "https://files.pythonhosted.org/packages/a7/da/fc25898c4edb9549b2aac0f7329fec027d654e94d4c4b89849d4c5fff0a4/dotty_dict-1.3.0.tar.gz"
+    sha256 "eb0035a3629ecd84397a68f1f42f1e94abd1c34577a19cd3eacad331ee7cbaf0"
   end
 
   resource "flake8" do
@@ -46,14 +56,19 @@ class Qmk < Formula
     sha256 "2838fd7200e5839ea4516ece953f3a19892c41089f0d933ba3f68e596aacfcd5"
   end
 
+  resource "jsonschema" do
+    url "https://files.pythonhosted.org/packages/69/11/a69e2a3c01b324a77d3a7c0570faa372e8448b666300c4117a516f8b1212/jsonschema-3.2.0.tar.gz"
+    sha256 "c8a85b28d377cc7737e46e2d9f2b4f44ee3c0e1deac6bf46ddefc7187d30797a"
+  end
+
   resource "mccabe" do
     url "https://files.pythonhosted.org/packages/06/18/fa675aa501e11d6d6ca0ae73a101b2f3571a565e0f7d38e062eec18a91ee/mccabe-0.6.1.tar.gz"
     sha256 "dd8d182285a0fe56bace7f45b5e7d1a6ebcbf524e8f3bd87eb0f125271b8831f"
   end
 
   resource "milc" do
-    url "https://files.pythonhosted.org/packages/95/06/09f898681a5a5ad2001639cbf7e998f32cba729a2ce439191f7058ce38c2/milc-1.0.10.tar.gz"
-    sha256 "62c4bbd8e3909d581db95bf8da5bad1122acfafb046c688fe1159d89333e37e0"
+    url "https://files.pythonhosted.org/packages/af/e3/14fade696e39b073a03aadb8703015e1b6e0d2218921c987ac23a8c1070a/milc-1.0.12.tar.gz"
+    sha256 "80080302bbc5420cd5df1bfde2b2ab06255571c6b7e0565fe8fb77ef45615d81"
   end
 
   resource "nose2" do
@@ -74,6 +89,16 @@ class Qmk < Formula
   resource "Pygments" do
     url "https://files.pythonhosted.org/packages/29/60/8ff9dcb5eac7f4da327ba9ecb74e1ad783b2d32423c06ef599e48c79b1e1/Pygments-2.7.3.tar.gz"
     sha256 "ccf3acacf3782cbed4a989426012f1c535c9a90d3a7fc3f16d231b9372d2b716"
+  end
+
+  resource "pyrsistent" do
+    url "https://files.pythonhosted.org/packages/4d/70/fd441df751ba8b620e03fd2d2d9ca902103119616f0f6cc42e6405035062/pyrsistent-0.17.3.tar.gz"
+    sha256 "2e636185d9eb976a18a8a8e96efce62f2905fea90041958d8cc2a189756ebf3e"
+  end
+
+  resource "setuptools-scm" do
+    url "https://files.pythonhosted.org/packages/af/df/f8aa8a78d4d29e0cffa4512e9bc223ed02f24893fe1837c6cee2749ebd67/setuptools_scm-5.0.1.tar.gz"
+    sha256 "c85b6b46d0edd40d2301038cdea96bb6adc14d62ef943e75afb08b3e7bcf142a"
   end
 
   resource "six" do

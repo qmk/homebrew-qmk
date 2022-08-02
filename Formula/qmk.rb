c476cc363bf301010ffd173c4fc4954f956d5d8f
hershey108 <hershiv.haria@gmail.com>
2022-08-02 22:02:17 +0100

    Adding libjpeg as dependency

--- Formula/qmk.rb
+++ Formula/qmk.rb
@@ -25,6 +25,7 @@ class Qmk < Formula
   depends_on "pillow"
   depends_on "python"
   depends_on "teensy_loader_cli"
+  depends_on "libjpeg"
 
   resource "appdirs" do
     url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
